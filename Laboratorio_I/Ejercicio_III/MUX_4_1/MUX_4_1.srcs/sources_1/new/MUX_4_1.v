6554023725ae02fc2589bfcf2c4edaad3649b20e
DOPALTEC <davidopereira@estudiantec.cr>
2024-07-28 21:08:23 -0600

    Mux_propuesto_sin_tb

--- Laboratorio_I/Ejercicio_III/MUX_4_1/MUX_4_1.srcs/sources_1/new/MUX_4_1.v
+++ Laboratorio_I/Ejercicio_III/MUX_4_1/MUX_4_1.srcs/sources_1/new/MUX_4_1.v
@@ -6,8 +6,17 @@ module MUX_4_1 #(parameter N=3)(
     input [N:0] IN_3,
     input [N:0] IN_4,
     input [1:0] sel,
-    output [N:0] OUT
+    output reg [N:0] OUT
     );
     
+always @(*) begin
+    case(sel)
+    2'b00: OUT=IN_1;
+    2'b01: OUT=IN_2;
+    2'b10: OUT=IN_3;
+    2'b11: OUT=IN_4;
+    endcase
+end
+
 
 endmodule
