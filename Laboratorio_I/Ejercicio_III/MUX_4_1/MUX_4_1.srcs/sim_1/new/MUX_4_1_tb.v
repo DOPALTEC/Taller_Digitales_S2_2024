f468c0084e29b12314a2d1032a546801265c2a68
DOPALTEC <davidopereira@estudiantec.cr>
2024-07-29 11:46:22 -0600

    Autoverificabilidad_de_tb_para_4bits

--- Laboratorio_I/Ejercicio_III/MUX_4_1/MUX_4_1.srcs/sim_1/new/MUX_4_1_tb.v
+++ Laboratorio_I/Ejercicio_III/MUX_4_1/MUX_4_1.srcs/sim_1/new/MUX_4_1_tb.v
@@ -27,6 +27,7 @@ initial begin
     IN_2=0;
     IN_3=0;
     IN_4=0;
+    //Para 4 bits
     repeat(50) begin
         sel=$random;
         IN_1=$random;
@@ -34,10 +35,27 @@ initial begin
         IN_3=$random;
         IN_4=$random;
         #20;
-  
+        
+        if (sel == 2'b00 && OUT != IN_1) begin
+            $display("Fallo en el Testbench: Selector: %b, IN_1: %b, Output: %b", sel, IN_1, OUT);
+        end
+        else if (sel == 2'b01 && OUT != IN_2) begin
+            $display("Fallo en el Testbench: Selector: %b, IN_2: %b, Output: %b", sel, IN_2, OUT);
+        end
+        else if (sel == 2'b10 && OUT != IN_3) begin
+            $display("Fallo en el Testbench: Selector: %b, IN_3: %b, Output: %b", sel, IN_3, OUT);
+        end
+        else if (sel == 2'b11 && OUT != IN_4) begin
+            $display("Fallo en el Testbench: Selector: %b, IN_4: %b, Output: %b", sel, IN_4, OUT);
+        end
+        else begin
+            $display("Iteracion Completada Correctamente");
+        end
     end
-
-
+    //Para 8 bits
+    
+    
+    
 
 end
 
