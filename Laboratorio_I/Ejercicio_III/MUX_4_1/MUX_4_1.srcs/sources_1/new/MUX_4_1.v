64e5d515260100507b38c1c8bf2fcd17d9f117cd
DOPALTEC <davidopereira@estudiantec.cr>
2024-07-28 21:04:22 -0600

    Parametrizacion_MUX

--- Laboratorio_I/Ejercicio_III/MUX_4_1/MUX_4_1.srcs/sources_1/new/MUX_4_1.v
+++ Laboratorio_I/Ejercicio_III/MUX_4_1/MUX_4_1.srcs/sources_1/new/MUX_4_1.v
@@ -1,10 +1,13 @@
 `timescale 1ns / 1ps
 
-module MUX_4_1(
-    input IN_1,
-    input IN_2,
-    input IN_3,
-    input IN_4,
-    output OUT
+module MUX_4_1 #(parameter N=3)(
+    input [N:0] IN_1,
+    input [N:0] IN_2,
+    input [N:0] IN_3,
+    input [N:0] IN_4,
+    input [1:0] sel,
+    output [N:0] OUT
     );
+    
+
 endmodule
