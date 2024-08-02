fe724727f7eb2af63206a4045329bf1ae7f8de1c
DOPALTEC <davidopereira@estudiantec.cr>
2024-08-02 17:07:42 -0600

    modulo para complemento a 2

---
+++ Laboratorio_I/Ejercicio_II/Switch_Button_Led/Switch_Button_Led.srcs/sources_1/new/Complemento_a_2.v
@@ -0,0 +1,15 @@
+`timescale 1ns / 1ps
+
+module Complemento_a_2(
+    input [3:0] sw,
+    output [3:0] led
+    );
+    
+    wire [3:0] comp_1;
+    wire [3:0] comp_2;
+    
+    assign comp_1 = ~sw;
+    assign comp_2 = comp_1 + 4'b0001;
+    assign led = comp_2;
+
+endmodule
