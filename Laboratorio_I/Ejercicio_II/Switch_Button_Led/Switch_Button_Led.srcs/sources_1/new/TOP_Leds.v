04811fe58b84994222f1df3d664ef01c8efe0ccf
DOPALTEC <davidopereira@estudiantec.cr>
2024-08-02 14:09:57 -0600

    Ejercicio_II_Iniciado

---
+++ Laboratorio_I/Ejercicio_II/Switch_Button_Led/Switch_Button_Led.srcs/sources_1/new/TOP_Leds.v
@@ -0,0 +1,27 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 02.08.2024 14:08:28
+// Design Name: 
+// Module Name: TOP_Leds
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module TOP_Leds(
+    input [3:0] sw,
+    output [3:0] led
+    );
+endmodule
