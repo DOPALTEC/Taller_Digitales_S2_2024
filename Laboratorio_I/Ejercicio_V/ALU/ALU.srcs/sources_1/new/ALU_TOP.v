f6355fe83b3727827ea3c2d6a88f4a4fb98658d7
DOPALTEC <davidopereira@estudiantec.cr>
2024-08-05 18:35:45 -0600

    Se inician los cases de la alu segun el control

--- Laboratorio_I/Ejercicio_V/ALU/ALU.srcs/sources_1/new/ALU_TOP.v
+++ Laboratorio_I/Ejercicio_V/ALU/ALU.srcs/sources_1/new/ALU_TOP.v
@@ -1,7 +1,21 @@
 `timescale 1ns / 1ps
 
-module ALU_TOP(
-    input [3:0] A,
-    input [3:0] B
-    );
+module ALU_TOP #(parameter N=3) (
+    input [N:0] A,
+    input [N:0] B,
+    input [N:0] ALUControl,
+    input ALUFlagIn,
+    output reg C,
+    output reg zero,
+    output reg [N:0] Y
+);
+
+always @* begin
+    case(ALUControl)
+        4'b0000: Y= A&B;
+    endcase
+
+end
+    
+    
 endmodule
