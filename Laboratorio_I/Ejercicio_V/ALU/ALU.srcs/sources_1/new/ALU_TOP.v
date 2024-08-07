09ec977a1627f7f55f7781b472e7900f3038d3b9
DOPALTEC <davidopereira@estudiantec.cr>
2024-08-06 21:49:24 -0600

    Avance en operaciones de la ALU

--- Laboratorio_I/Ejercicio_V/ALU/ALU.srcs/sources_1/new/ALU_TOP.v
+++ Laboratorio_I/Ejercicio_V/ALU/ALU.srcs/sources_1/new/ALU_TOP.v
@@ -12,7 +12,33 @@ module ALU_TOP #(parameter N=3) (
 
 always @* begin
     case(ALUControl)
-        4'b0000: Y= A&B;
+        4'b0000: Y= A&B; //AND
+        
+        4'b0001: Y= A|B; //OR
+        
+        4'b0010:  //SUMA COMPLENETO A 2
+            {C,Y} = A+B+ALUFlagIn;
+        4'b0011:  //INCREMENTO A 1
+            if (ALUFlagIn) begin
+                {C,Y}=B+4'b0001;
+            end
+            else begin
+                {C,Y}=A+4'b0001;
+            end
+        4'b0100: //DECREMENTO A 1
+            if (ALUFlagIn) begin
+                {C,Y}=B-4'b0001;
+            end
+            else begin
+                {C,Y}=A-4'b0001;
+            end
+        4'b0101: //NOT
+            if (ALUFlagIn) begin
+                Y=~B;
+            end
+            else begin
+                Y=~A;
+            end
     endcase
 
 end
