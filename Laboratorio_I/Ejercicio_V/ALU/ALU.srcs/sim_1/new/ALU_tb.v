7898e8124660f981f3f3158d4cad4f1708c57314
DOPALTEC <davidopereira@estudiantec.cr>
2024-08-11 23:48:39 -0600

    Correcciones en Testbench

--- Laboratorio_I/Ejercicio_V/ALU/ALU.srcs/sim_1/new/ALU_tb.v
+++ Laboratorio_I/Ejercicio_V/ALU/ALU.srcs/sim_1/new/ALU_tb.v
@@ -1,6 +1,6 @@
 `timescale 1ns / 1ps
 
-module ALU_TOP_TB;
+module ALU_tb;
     parameter N = 3;
 
     reg [N:0] A;
@@ -46,34 +46,79 @@ module ALU_TOP_TB;
             case (ALUControl)
                 4'b0000: expected_Y = A & B;  // AND
                 4'b0001: expected_Y = A | B;  // OR
-                4'b0010: expected_Y = A + B + ALUFlagIn;  // SUMA
+                4'b0010: {expected_C, expected_Y} = A + B + ALUFlagIn;  // SUMA
+                
                 4'b0011:  // INCREMENTO
-                if(ALUFlagIn)begin
-                    expected_Y = B + 1; 
+                if(ALUFlagIn) begin
+                    {expected_C, expected_Y} = B + 1; 
+                end else begin
+                    {expected_C, expected_Y} = A + 1; 
                 end
-                else begin
-                    expected_Y = A + 1; 
+                
+                4'b0100: // DECREMENTO
+                if(ALUFlagIn) begin
+                    {expected_C, expected_Y} = B - 1; 
+                end else begin
+                    {expected_C, expected_Y} = A - 1; 
                 end
-                4'b0100: expected_Y = A - 1;  // DECREMENTO
-                4'b0101: expected_Y = ~A;  // NOT
-                4'b0110: expected_Y = A - B - ALUFlagIn;  // RESTA
+
+                4'b0101:  // NOT
+                if(ALUFlagIn) begin
+                    expected_Y = ~B; 
+                end else begin
+                    expected_Y = ~A; 
+                end
+                
+                4'b0110: {expected_C, expected_Y} = A - B - ALUFlagIn;  // RESTA
+
                 4'b0111: expected_Y = A ^ B;  // XOR
-                4'b1000: expected_Y = A << 1;  // CORRIMIENTO IZQ
-                4'b1001: expected_Y = A >> 1;  // CORRIMIENTO DER
+
+                4'b1000:  // CORRIMIENTO IZQ
+                if (ALUFlagIn) begin
+                    if (B > N) begin
+                        expected_C = ALUFlagIn;
+                    end else begin
+                        expected_C = A[N+1-B];
+                    end 
+                    expected_Y = (A << B) | ({(N+1){1'b1}} >> ((N+1)-B));
+                end else begin
+                    if (B > N) begin
+                        expected_C = ALUFlagIn;
+                    end else begin
+                        expected_C = A[N+1-B];
+                    end  
+                    expected_Y = A << B;
+                end
+
+                4'b1001:  // CORRIMIENTO DER
+                if (ALUFlagIn) begin
+                    if (B > N) begin
+                        expected_C = ALUFlagIn;
+                    end else begin
+                        expected_C = A[N+1-B];
+                    end 
+                    expected_Y = (A >> B) | ({(N+1){1'b1}} << ((N+1)-B));
+                end else begin
+                    if (B > N) begin
+                        expected_C = ALUFlagIn;
+                    end else begin
+                        expected_C = A[N+1-B];
+                    end 
+                    expected_Y = A >> B;
+                end
+
                 default: expected_Y = 0;
             endcase
 
-            expected_C = (expected_Y > 15); 
             expected_Z = (expected_Y == 0);
 
             // Comparación y mensajes de error
-            //if (Y !== expected_Y || C !== expected_C || Z !== expected_Z) begin
-            if (((ALUControl==4'b0000 || ALUControl==4'b0001) && (Y !== expected_Y || Z !== expected_Z)) || ((ALUControl>=4'b0010 ) && (Y !== expected_Y || C !== expected_C || Z !== expected_Z))) begin
-                $display("Error: ALUControl=%b, A=%b, B=%b, Esperado: Y=%b, C=%b, Z=%b, Obtenido: Y=%b, C=%b, Z=%b", 
-                    ALUControl, A, B, expected_Y, expected_C, expected_Z, Y, C, Z);
+            if (Y !== expected_Y || C !== expected_C || Z !== expected_Z) begin
+                $display("Error: ALUControl=%b, A=%b, B=%b, ALUFlagIn=%b, Esperado: Y=%b, C=%b, Z=%b, Obtenido: Y=%b, C=%b, Z=%b", 
+                    ALUControl, A, B, ALUFlagIn, expected_Y, expected_C, expected_Z, Y, C, Z);
             end else begin
-                $display("Operación Exitosa: ALUControl=%b, A=%b, B=%b, Y=%b, C=%b, Z=%b", 
-                    ALUControl, A, B, Y, C, Z);
+                $display("Operación Exitosa: ALUControl=%b, A=%b, B=%b, ALUFlagIn=%b, Y=%b, C=%b, Z=%b", 
+                    ALUControl, A, B, ALUFlagIn, Y, C, Z);
             end
         end
 
