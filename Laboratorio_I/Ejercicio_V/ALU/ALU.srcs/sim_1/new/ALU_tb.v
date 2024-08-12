f11a7ffd4f507407566cd01a198d0e5b8c6490e7
DOPALTEC <davidopereira@estudiantec.cr>
2024-08-11 23:16:51 -0600

    Correcciones en Testbench

--- Laboratorio_I/Ejercicio_V/ALU/ALU.srcs/sim_1/new/ALU_tb.v
+++ Laboratorio_I/Ejercicio_V/ALU/ALU.srcs/sim_1/new/ALU_tb.v
@@ -23,84 +23,59 @@ module ALU_TOP_TB;
     );
 
     reg [31:0] seed; 
+    reg [N:0] expected_Y;
+    reg expected_C;
+    reg expected_Z;
+    integer i;  
+
     initial begin
-    
-        //Inicializar
-        //integer seed;
-        seed = 32'hDEADBEEF;
+        seed = 32'hDFADAEAF;
         
         A = 0;
         B = 0;
         ALUControl = 0;
         ALUFlagIn = 0;
-        
-        #10;
-        // AND
-        A = $random(seed) % 16;  // Genera un número aleatorio de 4 bits para A
-        B = $random(seed) % 16;  // Genera un número aleatorio de 4 bits para B
-        ALUControl = 4'b0000;
-        #10;
 
-        // OR
-        A = $random(seed) % 16;  // Genera un número aleatorio de 4 bits para A
-        B = $random(seed) % 16;  // Genera un número aleatorio de 4 bits para B
-        ALUControl = 4'b0001;
-        #10;
+        for (i = 0; i < 10; i = i + 1) begin
+            A = $random(seed) % 16;
+            B = $random(seed) % 16;
+            ALUFlagIn = $random(seed) % 2;
+            ALUControl = i;
+            #10;
 
-        // SUMA COMPLEMETO A 2
-        A = $random(seed) % 16;  // Genera un número aleatorio de 4 bits para A
-        B = $random(seed) % 16;  // Genera un número aleatorio de 4 bits para B
-        ALUFlagIn = $random(seed) % 2;
-        ALUControl = 4'b0010;
-        #10;
+            case (ALUControl)
+                4'b0000: expected_Y = A & B;  // AND
+                4'b0001: expected_Y = A | B;  // OR
+                4'b0010: expected_Y = A + B + ALUFlagIn;  // SUMA
+                4'b0011:  // INCREMENTO
+                if(ALUFlagIn)begin
+                    expected_Y = B + 1; 
+                end
+                else begin
+                    expected_Y = A + 1; 
+                end
+                4'b0100: expected_Y = A - 1;  // DECREMENTO
+                4'b0101: expected_Y = ~A;  // NOT
+                4'b0110: expected_Y = A - B - ALUFlagIn;  // RESTA
+                4'b0111: expected_Y = A ^ B;  // XOR
+                4'b1000: expected_Y = A << 1;  // CORRIMIENTO IZQ
+                4'b1001: expected_Y = A >> 1;  // CORRIMIENTO DER
+                default: expected_Y = 0;
+            endcase
 
-        // INCREMENTO A 1
-        A = $random(seed) % 16;  // Genera un número aleatorio de 4 bits para A
-        B = $random(seed) % 16;  // Genera un número aleatorio de 4 bits para B
-        ALUFlagIn = $random(seed) % 2;
-        ALUControl = 4'b0011;
-        #10;
+            expected_C = (expected_Y > 15); 
+            expected_Z = (expected_Y == 0);
 
-        // DECREMENTO A 1
-        A = $random(seed) % 16;  // Genera un número aleatorio de 4 bits para A
-        B = $random(seed) % 16;  // Genera un número aleatorio de 4 bits para B
-        ALUFlagIn = $random(seed) % 2;
-        ALUControl = 4'b0100;
-        #10;
-
-        // NOT
-        A = $random(seed) % 16;  // Genera un número aleatorio de 4 bits para A
-        B = $random(seed) % 16;  // Genera un número aleatorio de 4 bits para B
-        ALUFlagIn = $random(seed) % 2;
-        ALUControl = 4'b0101;
-        #10;
-
-        // RESTA COMPLEMENTO A 2
-        A = $random(seed) % 16;  // Genera un número aleatorio de 4 bits para A
-        B = $random(seed) % 16;  // Genera un número aleatorio de 4 bits para B
-        ALUFlagIn = $random(seed) % 2;
-        ALUControl = 4'b0110;
-        #10;
-
-        // XOR 
-        A = $random(seed) % 16;  // Genera un número aleatorio de 4 bits para A
-        B = $random(seed) % 16;  // Genera un número aleatorio de 4 bits para B
-        ALUControl = 4'b0111;
-        #10;
-
-        // CORRIMIENTO IZQ
-        A = $random(seed) % 16;  // Genera un número aleatorio de 4 bits para A
-        B = $random(seed) % 16;  // Genera un número aleatorio de 4 bits para B
-        ALUFlagIn = $random(seed) % 2;
-        ALUControl = 4'b1000;
-        #10;
-
-        // CORRIMIENTO DER
-        A = $random(seed) % 16;  // Genera un número aleatorio de 4 bits para A
-        B = $random(seed) % 16;  // Genera un número aleatorio de 4 bits para B
-        ALUFlagIn = $random(seed) % 2;
-        ALUControl = 4'b1001;
-        #10;
+            // Comparación y mensajes de error
+            //if (Y !== expected_Y || C !== expected_C || Z !== expected_Z) begin
+            if (((ALUControl==4'b0000 || ALUControl==4'b0001) && (Y !== expected_Y || Z !== expected_Z)) || ((ALUControl>=4'b0010 ) && (Y !== expected_Y || C !== expected_C || Z !== expected_Z))) begin
+                $display("Error: ALUControl=%b, A=%b, B=%b, Esperado: Y=%b, C=%b, Z=%b, Obtenido: Y=%b, C=%b, Z=%b", 
+                    ALUControl, A, B, expected_Y, expected_C, expected_Z, Y, C, Z);
+            end else begin
+                $display("Operación Exitosa: ALUControl=%b, A=%b, B=%b, Y=%b, C=%b, Z=%b", 
+                    ALUControl, A, B, Y, C, Z);
+            end
+        end
 
         $finish;
     end
