dcd1156bbf4ed75e533b4a51c4f9d2c3c141e116
DOPALTEC <davidopereira@estudiantec.cr>
2024-08-13 22:00:25 -0600

    Ejercicio2 Correccion

--- Laboratorio_I/Ejercicio_II/Switch_Button_Led/Switch_Button_Led.srcs/sources_1/new/Complemento_a_2.v
+++ Laboratorio_I/Ejercicio_II/Switch_Button_Led/Switch_Button_Led.srcs/sources_1/new/Complemento_a_2.v
@@ -1,15 +1,19 @@
 `timescale 1ns / 1ps
 
 module Complemento_a_2(
-    input [3:0] sw,
-    output [3:0] led
+    input [3:0] sw,  
+    output [3:0] led  
 );
     
-wire [3:0] comp_1;
-wire [3:0] comp_2;
+    reg [3:0] comp_2; 
+
+    always @(*) begin
+        if (sw[3] == 1) 
+            comp_2 = ~sw + 4'b0001; 
+        else
+            comp_2 = sw; 
+    end
     
-assign comp_1 = ~sw;
-assign comp_2 = comp_1 + 4'b0001;
-assign led = comp_2;
+    assign led = comp_2;
 
 endmodule
