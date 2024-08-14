87374aa25e95d1975a252a2fd19dbbd5e5656076
DOPALTEC <davidopereira@estudiantec.cr>
2024-08-14 11:39:39 -0600

    Correccion formato archivos

---
+++ Laboratorio_I/Ejercicio_III/src/MUX_4_1.v
@@ -0,0 +1,20 @@
+`timescale 1ns / 1ps
+
+module MUX_4_1 #(parameter N=3)(
+    input [N:0] IN_1,
+    input [N:0] IN_2,
+    input [N:0] IN_3,
+    input [N:0] IN_4,
+    input [1:0] sel,
+    output reg [N:0] OUT
+    );
+    
+always @(*) begin
+    case(sel)
+    2'b00: OUT=IN_1;
+    2'b01: OUT=IN_2;
+    2'b10: OUT=IN_3;
+    2'b11: OUT=IN_4;
+    endcase
+end
+endmodule
