268f44e40b2a53a191be21bd0b689d35b7050b7c
Kayu <ggonzalez02@estudiantec.cr>
2024-08-15 16:33:36 -0600

    nuevos cambios

--- Laboratorio_I/Ejercicio_IV/src/Ejercicio_IV.v
+++ Laboratorio_I/Ejercicio_IV/src/Ejercicio_IV.v
@@ -1,1 +1,26 @@
+module pwm_generador( 
+    input clk,
+    input [3:0] switch,  //Entrada de los switches 
+    output reg led
+    );
+
+    
+    localparam integer counter_MAX = 100_000 - 1;  // 100,000 clock cycles (maximo) es de este valor para un ancho de pulso de 1ms
+
+    
+    reg [16:0] counter = 0;  // regitro de 17 bits para poder realizar un contador que llegue a 100,000
+
+    always @(posedge clk) begin 
+        if (counter < counter_MAX) 
+            counter <= counter + 1; 
+        else 
+            counter <= 0; // vuelve el contador a cero 
+    end
+
+    // Ajuste del duty cycle del PWM por medio de los switches 
+    always @(posedge clk) begin
+        led <= (counter < (switch * 6667)) ? 1 : 0; 
+    end
+
+endmodule
 
