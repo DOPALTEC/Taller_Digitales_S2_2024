1444239b173d7f5fed0a3bfcd67942db29353777
DOPALTEC <davidopereira@estudiantec.cr>
2024-08-30 18:18:01 -0600

    Inicializar archivos Lab 2

---
+++ Laboratorio_II/Ejercicio_I/Ejercicio_I/Ejercicio_I.srcs/sources_1/new/Antirebotes.v
@@ -0,0 +1,27 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 30.08.2024 18:13:51
+// Design Name: 
+// Module Name: Antirebotes
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Antirebotes(
+    input clk,
+    input reset
+    );
+endmodule
