baf867aee6cdcac7192acca114e5cb27ddca8c2d
Maricruz Campos <maricruzcv1803@gmail.com>
2024-09-14 18:39:13 -0600

    Update debounce.v

--- Laboratorio_II/Ejercicio_I/src/debounce.v
+++ Laboratorio_II/Ejercicio_I/src/debounce.v
@@ -1,31 +1,42 @@
-`timescale 1ns / 1ps
 module debounce(
     input clk,
-    input rst_n,      
+    input reset,      
     input button_in,  // entrada del botón 
-    output debounced_out
+    output reg debounced_out
 );
-    parameter  DEBOUNCE_THRESHOLD = 1000000;  //  debounce delay
+
+    parameter  DEBOUNCE_THRESHOLD = 1;  // debounce delay
 
     reg [19:0] counter;  
-    reg button_state;    
-// contador 
-    always @(posedge clk or negedge rst_n) begin
-        if (!rst_n) begin
+    reg button_state;
+    reg button_stable;    // Reg para almacenar el estado estable
+    
+    // contador 
+    always @(posedge clk or negedge reset) begin
+        if (!reset) begin
             counter <= 0;
             button_state <= 0;
+            button_stable <= 0;  // Inicializar en 0
         end else begin
-            if (button_in != button_state) begin
+            if (button_in != button_stable) begin
                 counter <= counter + 1;
-                if (counter == DEBOUNCE_THRESHOLD) begin
-                    button_state <= button_in;
+                if (counter >= DEBOUNCE_THRESHOLD) begin
+                    button_stable <= button_in;   // Actualizar el estado estable
                     counter <= 0;
                 end
             end else begin
-                counter <= 0;
+                counter <= 0;  // Resetear el contador si button_in está estable
             end
         end
     end
 
-    assign debounced_out = button_state;
+    always @(posedge clk or negedge reset) begin
+        if (!reset) begin
+            debounced_out <= 0;
+        end else begin
+            debounced_out <= button_stable;  // Actualizar salida con el valor estable
+        end
+    end
+
 endmodule
+
