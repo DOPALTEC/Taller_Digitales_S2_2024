9c1522577ad2cd1be5f0c8d2f4a5ed97e1c15e4f
Maricruz Campos <maricruzcv1803@gmail.com>
2024-09-17 09:49:50 -0600

    Update counter.v

--- Laboratorio_II/Ejercicio_II/src/counter.v
+++ Laboratorio_II/Ejercicio_II/src/counter.v
@@ -1,14 +1,20 @@
-module two_bit_counter (
-    input wire clk,
-    input wire reset,
-    output reg [1:0] count
+`timescale 1ns / 1ps
+module contador(
+    input clk,
+    input reset,
+    input en,           // Señal de habilitación (enable)
+    output reg [1:0] q
 );
 
-always @(posedge clk or posedge reset) begin
-    if (reset)
-        count <= 2'b00;
-    else
-        count <= count + 1'b1;
-end
+    always @(posedge clk ) begin
+        if (!reset) begin
+            q <= 2'b00;     // Resetear el contador a 0
+        end
+        else if (en) begin
+            q <= q + 1'b1; // Incrementar el contador si el enable está activo
+        end
+        
+        // Si 'en' está en bajo, q mantiene su valor actual
+    end
 
 endmodule
