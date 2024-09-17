9c46d84f063248cd20722f13e800f3b497bb28a5
DOPALTEC <davidopereira@estudiantec.cr>
2024-09-17 00:45:47 -0600

    tb con dos dispositivos completado

--- Laboratorio_II/Ejercicio_III/UART/UART.srcs/sim_1/new/tb_uart.v
+++ Laboratorio_II/Ejercicio_III/UART/UART.srcs/sim_1/new/tb_uart.v
@@ -12,13 +12,15 @@ reg rst;
 reg [15:0] prescale;
 
 
-//////////////////SEÑALES PARA DISPOSITIVO #1////////////////////////
+//////////////////SENIALES PARA DISPOSITIVO #1////////////////////////
 //TX
 
 reg [DATA_WIDTH-1:0] s_axis_tdata;
 reg s_axis_tvalid;
 //wire txd;
 wire tx_busy;
+wire tx_to_rx_1_2;
+
 wire s_axis_tready;
 
 //RX
@@ -33,18 +35,19 @@ wire [DATA_WIDTH-1:0] m_axis_tdata;
 wire rx_overrun_error;
 wire rx_frame_error;
 
-//////////////////SEÑALES PARA DISPOSITIVO #2////////////////////////
+//////////////////SENIALES PARA DISPOSITIVO #2////////////////////////
 
 reg [DATA_WIDTH-1:0] s_axis_tdata_2;
 reg s_axis_tvalid_2;
-//wire txd_2;
 wire tx_busy_2;
+wire tx_to_rx_2_1;
 wire s_axis_tready_2;
+
+
 
 //RX
 wire m_axis_tvalid_2;
 reg m_axis_tready_2;
-//reg rxd_2;
 wire rx_busy_2;
 
 wire [DATA_WIDTH-1:0] m_axis_tdata_2;
@@ -52,14 +55,6 @@ wire [DATA_WIDTH-1:0] m_axis_tdata_2;
 
 wire rx_overrun_error_2;
 wire rx_frame_error_2;
-
-///////Generacion de Cables para Interconectar tx´s con rx´s
-
-wire tx_to_rx_1_2;
-wire tx_to_rx_2_1;
-
-//assign tx_to_rx_1_2 = txd;  // Conectar el transmisor del dispositivo 1 al receptor del dispositivo 2
-//assign tx_to_rx_2_1 = txd_2; // Conectar el transmisor del dispositivo 2 al receptor del dispositivo 1
 
 
 // DISPOSITIVO #1: Instancia del DUT (UART completo) 
@@ -84,10 +79,7 @@ uart_inst (
     .prescale(prescale)
 );
 
-
 //DISPOSITIVO #2
-
-
 uart #(
     .DATA_WIDTH(DATA_WIDTH)
 )
@@ -128,27 +120,21 @@ initial begin
     s_axis_tvalid_2 = 0;
     m_axis_tready = 0;
     m_axis_tready_2 = 0;
-    //rxd = 1;  // Inactivo (idle) es alto
-    //rxd_2 = 1;  // Inactivo (idle) es alto
-    prescale = 16'd1302; // Configuraciï¿½n de prescaler para tasa de baudios
-    seed = 32'h6C1322A8;
+    prescale = 16'd1302; // Configuracion de prescaler para tasa de baudios
+    seed = 32'hA54819CE;
     
-    //Inicializar senhales de verificacion
 
-    
-    ////////////////////////////////////////
-
-    // Liberaciï¿½n del reset despuï¿½s de un tiempo
+    // Liberacion del reset despues de un tiempo
     #20;
     rst = 0;
 
     // Espera para estabilizar
     #100;
 
-    // Prueba de transmisiï¿½n
- // Generaciï¿½n de valores aleatorios
-    s_axis_tdata = $random(seed);  // Dato a transmitir esperado aleatorio
-    s_axis_tdata_2 = $random(seed);   // Dato a recibir esperado aleatorio
+    // Prueba de transmision
+ // Generacion de valores aleatorios
+    s_axis_tdata = $random(seed);  // Dato a transmitir del dispositivo 1 esperado aleatorio
+    s_axis_tdata_2 = $random(seed);   // Dato a transmitir del dispositivo 2 esperado aleatorio
     
     
     
@@ -156,24 +142,14 @@ initial begin
     s_axis_tvalid = 1;  //Activa el envio de datos
     s_axis_tvalid_2 = 1;  //Activa el envio de datos
     
-    
     #937503;
     
     m_axis_tready = 1;  // Aceptar datos recibidos
     m_axis_tready_2 = 1;  // Aceptar datos recibidos
 
-    
-    //rxd = 0; // Start bit para receptor
-    //rxd_2 = 0; // Start bit para receptor
-    
-
-    s_axis_tvalid = 0;  // Finaliza la transmisiï¿½n de datos
-    s_axis_tvalid_2 = 0;  // Finaliza la transmisiï¿½n de datos
-    //#104167 rxd = 1;  // Stop bit
-    //rxd_2 = 1;  // Stop bit
-    //#104167;
-
-    // Espera para observar las seï¿½ales de salida
+    s_axis_tvalid = 0;  // Finaliza la transmision de datos
+    s_axis_tvalid_2 = 0;  // Finaliza la transmision de datos
+    // Espera para observar las seniales de salida
     #500;
     
     m_axis_tready = 0;  // Aceptar datos recibidos
