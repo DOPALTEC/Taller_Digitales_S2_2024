4d7f866827cbcc2891528109e5a5a85bfd477a8f
Maricruz Campos <maricruzcv1803@gmail.com>
2024-09-17 23:30:31 -0600

    Update top_module.v

--- Laboratorio_II/Ejercicio_II/src/top_module.v
+++ Laboratorio_II/Ejercicio_II/src/top_module.v
@@ -4,14 +4,17 @@ module top_module (
     input wire clk_in,           // Reloj de entrada
     input wire reset,            // Señal de reset
     input wire [3:0] external_in, // Entrada externa para el codificador
-    output wire [3:0] hex_out    // Salida combinada del registro de salida
+    output wire hex_out1,    // Salida combinada del registro de salida
+    output wire hex_out2,
+    output wire hex_out3,
+    output wire hex_out4,
+    output wire [1:0] counter_out
 );
 
     // Señales internas
     wire clk_div;                // Salida del divisor de reloj
     wire [1:0] debounced_signal; // Salida del debouncer
     wire sync_signal;            // Salida del sincronizador
-    wire [1:0] counter_out;      // Salida del contador
     wire [3:0] decoder_out;      // Salida del decodificador (2 a 4)
     wire [1:0] encoder_out;      // Salida del codificador (4 a 2)
 
@@ -39,7 +42,7 @@ module top_module (
 
     // Contador (habilitado por el sincronizador)
     contador counter_inst (
-        .clk(clk_div),
+        .clk(clk_in),
         .reset(reset),
         .en(sync_signal),
         .q(counter_out)
@@ -63,7 +66,11 @@ module top_module (
         .reset(reset),
         .counter_input(counter_out),
         .encoder_output(encoder_out),
-        .hex_out(hex_out)
+        .hex_out1(hex_out1),
+        .hex_out2(hex_out2),
+        .hex_out3(hex_out3),
+        .hex_out4(hex_out4)
     );
 
 endmodule
+
