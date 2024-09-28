d0807585fc4387b39ab2df243d01e156997c0096
Maricruz Campos <maricruzcv1803@gmail.com>
2024-09-28 03:28:43 -0600

    Update debounce.v

--- Laboratorio_II/Ejercicio_II/src/debounce/src/debounce.v
+++ Laboratorio_II/Ejercicio_II/src/debounce/src/debounce.v
@@ -4,14 +4,12 @@ module debounce (
     input clk,
     input reset,
     input key,  
-    output debounced_out         
+    output reg debounced_out         
 );
 
     reg key_rst;
     reg key_rst_r;
-    reg low_sw;
-    reg low_sw_r;
-    reg [7:0] cnt;  // Usamos 8 bits para contar hasta 200
+    reg [19:0] cnt;  
     
     // Flip-flop para almacenar el estado de la tecla
     always @(posedge clk) begin
@@ -31,38 +29,25 @@ module debounce (
     end
     
     // Señal para reiniciar el contador cuando hay un cambio en el key
-    wire cnt_rst = (~key_rst_r) & key_rst;
+    wire cnt_rst = (key_rst_r != key_rst);
 
     // Contador
     always @(posedge clk) begin
         if (!reset) begin
-            cnt <= 8'd0;  // Reinicio del contador
+            cnt <= 20'd0;  // Reinicio del contador
         end else if (cnt_rst) begin
-            cnt <= 8'd0;  // Reinicia si hay un cambio en la tecla
-        end else if (cnt < 8'd200) begin  // Solo incrementa hasta 200
+            cnt <= 20'd0;  // Reinicia si hay un cambio en la tecla
+        end else if (cnt < 20'd1000000) begin  
             cnt <= cnt + 1'b1;  // Incrementa el contador
-        end
-    end
-    
-    // Lógica para actualizar low_sw cuando el contador alcanza 200
-    always @(posedge clk) begin
+        end    
+    // Lógica para activar debounced_out
         if (!reset) begin
-            low_sw <= 1'b0;
-        end else if (cnt == 8'd200) begin  // Cuando el contador alcanza 200
-            low_sw <= key;
-        end
-    end
-    
-    // Registro para la salida retrasada de low_sw
-    always @(posedge clk) begin
-        if (!reset) begin
-            low_sw_r <= 1'b0;
+            debounced_out <= 1'b0;  // Reinicia la salida
+        end else if (cnt == 20'd1000000) begin  
+            debounced_out <= 1'b1;;  // Se activa después de estabilizarse
         end else begin
-            low_sw_r <= low_sw;
+            debounced_out <= 1'b0;  // Reinicia debounced_out si hay un cambio
         end
     end
-    
-    // Salida debounced
-    assign debounced_out = (~low_sw_r) & low_sw;  // Activa cuando hay un cambio en low_sw
 
 endmodule
