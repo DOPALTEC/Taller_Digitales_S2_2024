875a33af0b9816d029461647559c499e2d66cff5
Maricruz Campos <maricruzcv1803@gmail.com>
2024-09-30 14:50:51 -0600

    Update cont.v

--- Laboratorio_II/Ejercicio_I/src/cont.v
+++ Laboratorio_II/Ejercicio_I/src/cont.v
@@ -1,33 +1,15 @@
-`timescale 1ns / 1ps
-
-module counter (
-    input clk,
-    input reset,
-    input enable,  // Enable signal from debounce
-    output reg [7:0] count  // Salida del contador
+`timescale 1ms / 100us
+module counter_8bit (
+    input wire clk,
+    input wire rst_n,
+    input wire key_pressed, //Representa el enable 
+    output reg [7:0] count
 );
-
-    // Registro para almacenar el estado de enable
-    reg enable_latch;
-
-    always @(posedge clk) begin
-        if (!reset) begin
-            count <= 8'b00000000;  // Reinicia el contador
-            enable_latch <= 1'b0;  // Reinicia el latch de enable
-        end else if (enable) begin
-            enable_latch <= 1'b1;  // Activa el latch cuando enable es 1
-        end else if (!enable && enable_latch) begin
-            enable_latch <= 1'b0;  // Desactiva el latch cuando enable vuelve a 0
+    always @(posedge clk or negedge rst_n) begin
+        if (!rst_n) begin
+            count <= 8'b00000000;
+        end else if (!key_pressed) begin  // Cuenta cuando no hay ninguna tecla presionada
+            count <= count + 1;
         end
     end
-
-    // Incrementar el contador si el latch no está activado
-    always @(posedge clk) begin
-        if (!reset) begin
-            count <= 8'b00000000;  // Reinicia el contador
-        end else if (!enable_latch) begin
-            count <= count + 1'b1;  // Incrementa el contador
-        end
-    end
-
 endmodule
