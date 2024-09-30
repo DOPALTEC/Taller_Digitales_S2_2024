d388dcbb478c1bdbe2231734b61173db989972b7
Maricruz Campos <maricruzcv1803@gmail.com>
2024-09-30 14:37:12 -0600

    Update counter.v

--- Laboratorio_II/Ejercicio_II/interfaz/src/counter.v
+++ Laboratorio_II/Ejercicio_II/interfaz/src/counter.v
@@ -2,15 +2,14 @@
 module counter_2bit (
     input wire clk,
     input wire rst_n,
-    input wire key_pressed,  // Changed from key_valid to key_pressed
+    input wire key_pressed,  
     output reg [1:0] count
 );
     always @(posedge clk or negedge rst_n) begin
         if (!rst_n) begin
             count <= 2'b00;
-        end else if (!key_pressed) begin  // Count when key is not pressed
+        end else if (!key_pressed) begin  // Cuenta cuando no hay ninguna tecla presionada
             count <= count + 1;
         end
     end
 endmodule
-
