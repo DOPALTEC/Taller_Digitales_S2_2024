2afe7475ac7bff5564eee270826db27d63a41019
DOPALTEC <davidopereira@estudiantec.cr>
2024-10-08 16:37:47 -0600

    uart nexysn inicializado

---
+++ Laboratorio_II/Ejercicio_III/UART_Nexys/UART_Nexys.srcs/sources_1/new/UART_Nexys.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 08.10.2024 16:36:51
+// Design Name: 
+// Module Name: UART_Nexys
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module UART_Nexys(
+
+    );
+endmodule
