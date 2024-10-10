45bf7696798aa426ab17eaf5ac9df8f0ffe9fdea
DOPALTEC <davidopereira@estudiantec.cr>
2024-10-10 02:36:04 -0600

    Interfaz UART avance

--- Laboratorio_III/UART_Nexys/UART_Nexys.srcs/sources_1/new/DeMUX.v
+++ Laboratorio_III/UART_Nexys/UART_Nexys.srcs/sources_1/new/DeMUX.v
@@ -1,26 +1,26 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 10.10.2024 01:30:33
-// Design Name: 
-// Module Name: DeMUX
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module DeMUX(
-
+    input wire wr_i,        // Bit de entrada
+    input wire reg_sel_i,   // Bit de control
+    output reg WR1_reg_ctrl, // Salida cuando reg_sel_i = 0
+    output reg WR1_reg_data  // Salida cuando reg_sel_i = 1
     );
+    
+always @(*) begin
+    // Inicializamos ambas salidas a 0
+    WR1_reg_ctrl = 0;
+    WR1_reg_data = 0;
+    
+    // Demux en función de reg_sel_i
+    if (reg_sel_i == 0) begin
+        WR1_reg_ctrl = wr_i;  // Paso de wr_i cuando reg_sel_i es 0
+    end else begin
+        WR1_reg_data = wr_i;  // Paso de wr_i cuando reg_sel_i es 1
+    end
+end
+
 endmodule
+
+
+
--- Laboratorio_III/UART_Nexys/UART_Nexys.srcs/sources_1/new/Interfaz_UART_Nexys.v
+++ Laboratorio_III/UART_Nexys/UART_Nexys.srcs/sources_1/new/Interfaz_UART_Nexys.v
@@ -1,26 +1,41 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 10.10.2024 01:16:41
-// Design Name: 
-// Module Name: Interfaz_UART_Nexys
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
 
-module Interfaz_UART_Nexys(
-
+//module Interfaz_UART_Nexys #(parameter Palabra = 32) (
+//------------AL FINALIZAR PRUEBAS CAMBIAR A 32-------------------//
+module Interfaz_UART_Nexys #(parameter Palabra = 8) (
+    input  wire CLK100MHZ,
+    input  wire rst,
+    
+    input wire wr_i, //usar switch
+    input wire reg_sel_i, //usar switch
+    
+    input wire [Palabra-1:0]  entrada_i, //USAR SWITCHES
+    input wire addr_i, //usar switch
+    
+    output wire [Palabra-1:0]  salida_o //Usar LEDS 
     );
+    
+reg WR1_reg_ctrl;
+reg WR1_reg_data;
+    
+DeMUX DeMUX_inst(
+    .wr_i(wr_i),        // Bit de entrada
+    .reg_sel_i(reg_sel_i),   // Bit de control
+    .WR1_reg_ctrl(WR1_reg_ctrl), // Salida cuando reg_sel_i = 0
+    .WR1_reg_data(WR1_reg_data) 
+);
+
+
+Reg_Data Reg_Data_inst(
+    IN1(entrada_i),       // Entrada 1 de 32 bits
+    IN2(),       // Entrada 2 de 32 bits Proveniente de UART
+    addr1(addr_i),            // Línea de dirección para IN1
+    addr2(),            // Línea de dirección para IN2 Proveniente de UART
+    WR1(wr_i),              // Bit de escritura para IN1
+    WR2(),              // Bit de escritura para IN2 Proveniente de UART
+    hold_ctrl(),        // Control para seleccionar el registro Proveniente de UART
+    OUT()  
+);
+    
 endmodule
