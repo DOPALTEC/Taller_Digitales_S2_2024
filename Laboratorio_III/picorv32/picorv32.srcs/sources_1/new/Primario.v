9ebec86a4e01eb7383b975b53b6b833deae1c6a4
DOPALTEC <davidopereira@estudiantec.cr>
2024-10-20 23:05:45 -0600

    Instanciacion de rv32 avance

--- Laboratorio_III/picorv32/picorv32.srcs/sources_1/new/Primario.v
+++ Laboratorio_III/picorv32/picorv32.srcs/sources_1/new/Primario.v
@@ -1,26 +1,51 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 20.10.2024 21:27:48
-// Design Name: 
-// Module Name: Primario
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
 
-module Primario(
-
+module Primario #(parameter 
+ENABLE_COMPRESSED=0, 
+ENABLE_MUL=0, 
+ENABLE_DIV=0, 
+ENABLE_IRQ=0, 
+ENABLE_FAST_MUL=0,
+ENABLE_IRQ_QREGS=0 )(
+    input  wire clk,
+    input  wire rst
     );
+    
+    wire locked;
+    wire CLK_200MHZ;
+    
+   CLK_Gen CLK_Gen_inst
+   (
+    .CLK_100MHZ(clk), //input de 100mhz
+    .reset(rst),
+    .locked(locked),
+    .CLK_200MHZ(CLK_200MHZ));//output de 200mhz
+
+    
+    picorv32 #(
+		.STACKADDR(STACKADDR),
+		.PROGADDR_RESET(PROGADDR_RESET),
+		.PROGADDR_IRQ(PROGADDR_IRQ),
+		.BARREL_SHIFTER(BARREL_SHIFTER),
+		.COMPRESSED_ISA(ENABLE_COMPRESSED), //Instrucciones comprimidas no se usa (Deshabilitado 0)
+		.ENABLE_COUNTERS(ENABLE_COUNTERS),
+		.ENABLE_MUL(ENABLE_MUL), //Multiplicador (deshabilitado 0)
+		.ENABLE_DIV(ENABLE_DIV), //Divisor (deshabilitado 0)
+		.ENABLE_FAST_MUL(ENABLE_FAST_MUL),//Multiplicacion Rapida (deshabilitado 0)
+		.ENABLE_IRQ(ENABLE_IRQ), //Interrupcion (deshabilitado 0)
+		.ENABLE_IRQ_QREGS(ENABLE_IRQ_QREGS) //Interrupcion Registros (deshabilitado 0)
+	) cpu (
+		.clk(CLK_200MHZ),
+		.resetn(rst),
+		.mem_valid   (mem_valid  ),
+		.mem_instr   (mem_instr  ),
+		.mem_ready   (mem_ready  ),
+		.mem_addr    (mem_addr   ),
+		.mem_wdata   (mem_wdata  ),
+		.mem_wstrb   (mem_wstrb  ),
+		.mem_rdata   (mem_rdata  ),
+		.irq         (irq        )
+	);
+    
 endmodule
