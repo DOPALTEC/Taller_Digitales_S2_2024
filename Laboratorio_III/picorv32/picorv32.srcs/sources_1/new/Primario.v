d7431117afd567d171a99e90d08c5b932f0240da
DOPALTEC <davidopereira@estudiantec.cr>
2024-10-21 00:38:42 -0600

    Instanciacion de rv32 avance

--- Laboratorio_III/picorv32/picorv32.srcs/sources_1/new/Primario.v
+++ Laboratorio_III/picorv32/picorv32.srcs/sources_1/new/Primario.v
@@ -2,12 +2,17 @@
 
 
 module Primario #(parameter 
-ENABLE_COMPRESSED=0, 
-ENABLE_MUL=0, 
-ENABLE_DIV=0, 
-ENABLE_IRQ=0, 
-ENABLE_FAST_MUL=0,
-ENABLE_IRQ_QREGS=0 )(
+    STACKADDR=32'h0007_FFFF, //Posicion especificada en el mapa de memoria para la ram (memoria de datos)
+    PROGADDR_RESET=32'h 0000_0000,//Posicion incial de la memoria de programa
+    PROGADDR_IRQ=0,
+    BARREL_SHIFTER=1,
+    ENABLE_COMPRESSED=0, 
+    ENABLE_COUNTERS=1,
+    ENABLE_MUL=0, 
+    ENABLE_DIV=0,
+    ENABLE_FAST_MUL=0, 
+    ENABLE_IRQ=0, 
+    ENABLE_IRQ_QREGS=0)(
     input  wire clk,
     input  wire rst
     );
@@ -15,18 +20,34 @@ ENABLE_IRQ_QREGS=0 )(
     wire locked;
     wire CLK_200MHZ;
     
-   CLK_Gen CLK_Gen_inst
-   (
+//INSTANCIACION DE CLK 200 MHZ
+CLK_Gen CLK_Gen_inst(
     .CLK_100MHZ(clk), //input de 100mhz
     .reset(rst),
     .locked(locked),
-    .CLK_200MHZ(CLK_200MHZ));//output de 200mhz
+    .CLK_200MHZ(CLK_200MHZ)//output de 200mhz
+);
+
+//INSTANCIACION DE ROM
+ROM ROM_inst (
+  .a(a),      // input wire [8 : 0] Address, direccion de ROM
+  .spo(spo)  // output wire [31 : 0] spo
+);
+
+RAM RAM_inst (
+  .a(a),      // input wire [14 : 0] a Direccion de RAM
+  .d(d),      // input wire [31 : 0] Entrada dato a almacenar
+  .clk(CLK_200MHZ), 
+  .we(we),    // input Escritura en Memoria de Datos
+  .spo(spo)  // output wire [31 : 0] spo
+);
+
 
     
     picorv32 #(
-		.STACKADDR(STACKADDR),
-		.PROGADDR_RESET(PROGADDR_RESET),
-		.PROGADDR_IRQ(PROGADDR_IRQ),
+		.STACKADDR(STACKADDR),//Direccion de la Pila
+		.PROGADDR_RESET(PROGADDR_RESET), //Direccion inicial, al resetear
+		.PROGADDR_IRQ(PROGADDR_IRQ), //Interrupciones Addres Programa (deshabilitado 0)
 		.BARREL_SHIFTER(BARREL_SHIFTER),
 		.COMPRESSED_ISA(ENABLE_COMPRESSED), //Instrucciones comprimidas no se usa (Deshabilitado 0)
 		.ENABLE_COUNTERS(ENABLE_COUNTERS),
