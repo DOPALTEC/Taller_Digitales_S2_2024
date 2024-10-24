3628527a0bbd797a01faa39f548a42932c0d9804
DOPALTEC <davidopereira@estudiantec.cr>
2024-10-23 21:34:35 -0600

    instrucciones li y sw pruebas

--- Laboratorio_III/picorv32/picorv32.srcs/sources_1/new/Primario.v
+++ Laboratorio_III/picorv32/picorv32.srcs/sources_1/new/Primario.v
@@ -19,21 +19,35 @@ module Primario #(parameter
 
 wire [31:0] mem_addr; //Direccion de memoria que lee 
 wire  [31:0] mem_rdata; //Lee la instruccion
-
+/*
+Genera un desplazamiento a la derecha que dimensiona los saltos en 32 bits y no por bytes
+Divide entre 4 el valor de las direcciones
+*/
 wire [11:0] rom_addr = mem_addr[13:2]; 
+wire  [31:0] rom_rdata;
 //Como la ROM termina en 0x0fff se puede acotar
 ROM ROM_inst (
   .a(rom_addr),      // input wire [8 : 0] 
-  .spo(mem_rdata)  // output wire [31 : 0] spo
+  .spo(rom_rdata)  // output wire [31 : 0] spo
 );
 
 
+wire  [31:0] ram_rdata;
+reg ram_we;
+wire [14:0] ram_addr = mem_addr[15:2]; //Escala de bytes a 1 palabra
+
+reg [31:0] ram_wdata;
+/*
+No hay problema que una misma address caiga en ambas memorias
+we determina si se escribe en ram o no
+*/
+
 RAM RAM_inst (
-  .a(a),      // input wire [14 : 0] a
-  .d(d),      // input wire [31 : 0] d
+  .a(ram_addr),      // input wire [14 : 0] a
+  .d(ram_wdata),      // input wire [31 : 0] d
   .clk(clk),  // input wire clk
-  .we(we),    // input wire we
-  .spo(spo)  // output wire [31 : 0] spo
+  .we(ram_we),    // input wire we
+  .spo(ram_rdata)  // output wire [31 : 0] spo
 );
 
 /////////////////////INSTANCIACION DE RV32////////////////////////////
@@ -59,13 +73,28 @@ picorv32 #() cpu (
     .mem_rdata(mem_rdata)//Lee lo que hay en la memoria
 );
 
+/*
+Si se trata de un acceso a memoria de instruccion (mem_instr=1) 
+lee la salida de la ROM caso contrario lee los datos de la RAM
+*/
+assign mem_rdata = (mem_instr) ? rom_rdata : ram_rdata; // Selecciona entre ROM o RAM para lecturas
+
+
+
+
 always @(posedge clk or posedge rst) begin
     if (rst) begin
         mem_ready <= 0; // Inicializa mem_ready en 0 al reset
+        ram_we<=0;
     end else begin
         mem_ready <= 0; // Por defecto, mem_ready es 0
+        ram_we<=0;
         if (mem_valid && !mem_ready) begin
             mem_ready <= 1; // Habilita mem_ready si hay operación válida
+            if(!mem_instr && (mem_wstrb[0] || mem_wstrb[1]) || mem_wstrb[2] || mem_wstrb[3])begin
+                ram_wdata <= mem_wdata; // Asigna los datos a escribir en RAM
+                ram_we <= 1; // Habilita escritura en RAM
+            end
         end
     end
 end
