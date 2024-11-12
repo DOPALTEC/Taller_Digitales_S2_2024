03915c5eaec5a4293ce1098e8c2a6c4685a90f02
DOPALTEC <davidopereira@estudiantec.cr>
2024-11-12 03:32:52 -0600

    Instanciacion bloque UART_B

--- Laboratorio_III/picorv32/picorv32.srcs/sources_1/new/Primario.v
+++ Laboratorio_III/picorv32/picorv32.srcs/sources_1/new/Primario.v
@@ -1,19 +1,7 @@
 `timescale 1ns / 1ps
 
 
-module Primario #(parameter palabra=32, parameter prescale=1302
-    //STACKADDR=32'h0007_FFFF, // Posicion especificada en el mapa de memoria para la RAM
-    //PROGADDR_RESET=32'h 0000_0000, // Posicion inicial de la memoria de programa
-    //PROGADDR_IRQ=0,
-    //BARREL_SHIFTER=0,
-    //ENABLE_COMPRESSED=0, 
-    //ENABLE_COUNTERS=0,
-    //ENABLE_MUL=0, 
-    //ENABLE_DIV=0,
-    //ENABLE_FAST_MUL=0, 
-    //ENABLE_IRQ=0, 
-    //ENABLE_IRQ_QREGS=0
-    )
+module Primario #(parameter palabra=32, parameter prescale=1302)
 (
     input  wire clk,
     input  wire rst,
@@ -93,33 +81,58 @@ picorv32 cpu (
 );
 
 
-
-
-// Instancia del módulo Interfaz_UART_Nexys
-reg wr_i;
-reg reg_sel_i;
-wire [31:0] ctrl;
-wire [31:0] data;
-reg [31:0] entrada_i;
-reg addr_i;
-reg [31:0] entrada_i_data;
+/////PROTOCOLO UART_A (NEXYS/PC)//////////////
+reg wr_i_A;
+reg reg_sel_i_A;
+wire [31:0] ctrl_A;
+wire [31:0] data_A;
+reg [31:0] entrada_i_A;
+reg addr_i_A;
+reg [31:0] entrada_i_data_A;
 
 Interfaz_UART_Nexys #(
     .palabra(palabra),
     .prescale(prescale)
-) interfaz_uart_inst (
+) interfaz_uart_inst_A (
     .clk(clk),                // Reloj de entrada
     .rst(rst),                // Reset
-    .wr_i(wr_i),              // Señal de escritura
-    .reg_sel_i(reg_sel_i),    // Señal de selección de registro
-    .addr_i(addr_i),          // Dirección
-    .entrada_i(entrada_i),    // Entrada de control
-    .entrada_i_data(entrada_i_data), // Entrada de datos
-    .ctrl(ctrl),              // Salida de control
-    .data(data),              // Salida de datos
+    .wr_i(wr_i_A),              // Señal de escritura
+    .reg_sel_i(reg_sel_i_A),    // Señal de selección de registro
+    .addr_i(addr_i_A),          // Dirección
+    .entrada_i(entrada_i_A),    // Entrada de control
+    .entrada_i_data(entrada_i_data_A), // Entrada de datos
+    .ctrl(ctrl_A),              // Salida de control
+    .data(data_A),              // Salida de datos
     .rxd(rxd),                // RX de entrada
     .txd(txd)                 // TX de salida
 );
+
+/////PROTOCOLO UART_B (NEXYS/TANG)//////////////
+
+reg wr_i_B;
+reg reg_sel_i_B;
+wire [31:0] ctrl_B;
+wire [31:0] data_B;
+reg [31:0] entrada_i_B;
+reg addr_i_B;
+reg [31:0] entrada_i_data_B;
+
+Interfaz_UART_Nexys #(
+    .palabra(palabra),
+    .prescale(prescale)
+) interfaz_uart_inst_B ( 
+    .clk(clk),                // Reloj de entrada
+    .rst(rst),                // Reset
+    .wr_i(wr_i_B),              // Señal de escritura
+    .reg_sel_i(reg_sel_i_B),    // Señal de selección de registro
+    .addr_i(addr_i_B),          // Dirección
+    .entrada_i(entrada_i_B),    // Entrada de control
+    .entrada_i_data(entrada_i_data_B), // Entrada de datos
+    .ctrl(ctrl_B),              // Salida de control
+    .data(data_B),              // Salida de datos
+    .rxd(rxd_B),                // RX de entrada
+    .txd(txd_B)                 // TX de salida
+);                 
 
 
 /*
@@ -131,8 +144,8 @@ lee la salida de la ROM caso contrario lee los datos de la RAM
 // Asignación de mem_rdata
 assign mem_rdata = 
                    (mem_addr == 32'h2000) ? {16'b0, SW} :
-                   (mem_addr == 32'h2010) ? {24'b0, ctrl} :
-                   (mem_addr == 32'h201C && ctrl[1]) ? {24'b0, data} :
+                   (mem_addr == 32'h2010) ? {24'b0, ctrl_A} :
+                   (mem_addr == 32'h201C && ctrl_A[1]) ? {24'b0, data_A} :
                    (mem_instr ? rom_rdata : ram_rdata);
 /*En base a mem_valid para cada uno de los perifericos debe haber un mem_ready, multiplexar 
 tambien quien envia esto
@@ -164,26 +177,26 @@ reg reg_sel_i_delay;
 
 always @(posedge clk or posedge rst) begin
     if (rst) begin
-        wr_i <= 0;
-        reg_sel_i <= 0;
-        entrada_i <= 0;
-        addr_i <= 0;
+        wr_i_A <= 0;
+        reg_sel_i_A <= 0;
+        entrada_i_A <= 0;
+        addr_i_A <= 0;
         ena<=0;
     end else begin
         if (mem_valid && mem_addr == 32'h2010) begin
-            entrada_i <= mem_wdata; // Asigna el dato de memoria a entrada_i
-            wr_i <= 1;              // Señal de escritura en 1
-            reg_sel_i <= 0;         // Señal de selección en 0
+            entrada_i_A <= mem_wdata; // Asigna el dato de memoria a entrada_i
+            wr_i_A <= 1;              // Señal de escritura en 1
+            reg_sel_i_A <= 0;         // Señal de selección en 0
         end 
         else if (mem_valid && mem_addr == 32'h2018)begin
-            entrada_i_data <= mem_wdata; // Asigna el dato de memoria a entrada_i
-            wr_i <= 1;              // Señal de escritura en 1
-            reg_sel_i <= 1;         // Señal de selección en 1
-            addr_i <= 0;
+            entrada_i_data_A <= mem_wdata; // Asigna el dato de memoria a entrada_i
+            wr_i_A <= 1;              // Señal de escritura en 1
+            reg_sel_i_A <= 1;         // Señal de selección en 1
+            addr_i_A <= 0;
         end
-        else if (mem_valid && mem_addr == 32'h201C && ctrl[1] == 1) begin
-            addr_i <= 1;           // Asigna addr_i a 1 si se cumple la condición
-            reg_sel_i <= 1;        // Señal de selección en 1
+        else if (mem_valid && mem_addr == 32'h201C && ctrl_A[1] == 1) begin
+            addr_i_A <= 1;           // Asigna addr_i a 1 si se cumple la condición
+            reg_sel_i_A <= 1;        // Señal de selección en 1
             addr_i_delay <= 1;     // Activa el retardo
             reg_sel_i_delay <= 1;
         end
@@ -191,11 +204,11 @@ always @(posedge clk or posedge rst) begin
             LED<=mem_wdata[15:0];
         end
         else begin
-            wr_i <= 0; 
-            reg_sel_i<=0;
-            addr_i<=0;    
-            reg_sel_i <= reg_sel_i_delay;
-            addr_i <= addr_i_delay;
+            wr_i_A <= 0; 
+            reg_sel_i_A<=0;
+            addr_i_A<=0;    
+            reg_sel_i_A <= reg_sel_i_delay;
+            addr_i_A <= addr_i_delay;
             reg_sel_i_delay <= 0; // Desactiva el retardo después de un ciclo
             addr_i_delay <= 0;
         end
